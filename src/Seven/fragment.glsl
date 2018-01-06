66599089ef16ba3b531d2587973c9aa8590531af
RGBboy <l-_-l@rgbboy.com>
2018-01-06 20:39:26 +1300

    refactor;

--- src/Seven/fragment.glsl
+++ src/Seven/fragment.glsl
@@ -1,12 +1,11 @@
 precision mediump float;
 
-varying vec3 vNormal;
 varying vec3 vPosition;
 
 void main() {
-  float test = ceil((vPosition.x + vPosition.y + vPosition.z) * 5.0);
+  float surface = ceil((vPosition.x + vPosition.y + vPosition.z) * 5.0);
   float col = 1.0;
-  if (mod(test, 2.0) == 0.0) {
+  if (mod(surface, 2.0) == 0.0) {
     col = 0.0;
   }
   gl_FragColor = vec4(vec3(col), 1.0);
